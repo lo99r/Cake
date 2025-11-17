0f08498904bd0349421bef03861f7e70e2d275e6
MONIKAS STOUDIAS FuchsuaProject FrameDOS Project <lookout1423@gmail.com>
2025-11-17 22:52:19 +0800

    Tmporary: uefi main.asm에 findroot 함수 추가 sources/uefi/main.asm에서 findroot 함수의 초기형태를 작성하였습니다.' '

--- sources/uefi/main.asm
+++ sources/uefi/main.asm
@@ -3,20 +3,70 @@ default rel
 global main
 
 section .data
-guid:
+BlockIoProtocolGuid:
 dd 0x964e5b21
 dw 0x6459, 0x11d2
 dq 0x3b7269c9a000398e
+LoadedImageProtocolGuid:
+dd 0x5B1B31A1, 0x11D29562
+dq 0x3B7269C9A0003F8E
 
 HandleBuffer: dq 0
 HandleCount: dq 0
 BufferOfSector: times 512 db 0
 BlockIO: dq 0
 Maple: times 4096 db 0
+LoadedImage: dq 0
+;BlockIo: dq 0
+Media: dq 0
+BlockSize: dd 0
+LastBlock: dq 0
+MediaId: dd 0
+Removable: db 0
 
 section .text
+find_root: ;void find_root(EFI_HANDLE *ImageHandle(rcx), EFI_SYSTEM_TABLE *SystemTable(rdx), void *Buffer(r8));
+    sub rsp, 0x28
+    mov rax, [rdx+0x98]
+    push rcx
+    push rdx
+    push r8
+    ;push r9
+    lea rdx, [LoadedImageProtocolGuid]
+    lea r8, [LoadedImage]
+    call rax
+    mov rbx, [LoadedImage]
+    add rbx, 0x18
+    mov rcx, rbx
+    mov rsi, [rsp+8]
+    mov r8, [BlockIO]
+    mov rax, [rsi+0x98]
+    call rax
+    mov r12, [BlockIO]
+    add r12, 0x8
+    mov r13, [r12]
+    add r13, 9
+    mov [BlockSize], r13
+    mov r12, [BlockIO]
+    add r12, 8
+    mov r13, [r12]
+    add r13, 18
+    mov [LastBlock], r13
+    mov r12, [BlockIO]
+    add r12, 8
+    mov r13, [r12]
+    mov [MediaId], r13
+    mov r12, [BlockIO]
+    add r12, 8
+    mov r13, [r12]
+    add r13, 5
+    mov [Removable], r13
+
+
+
 read_sector:
     ;-
 
 main:
     ;-
+ㅌㅌㅌ
